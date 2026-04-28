7523645f48e95ee1faa88479e4f00b82ecc2e38b
arumullagunasekhar04-alt <arumullagunasekhar04@gmail.com>
2026-04-28 13:59:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,34 @@
 const SUPABASE_URL = "https://cxrofosdlooeyniddhdp.supabase.co/rest/v1/"
 const SUPABASE_KEY = "sb_publishable_x2VXaOaw-K_ej6KvV-StEw_QXWqvHBq"
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+
+async function loadProducts(){
+
+const { data } = await client
+.from("products")
+.select("*")
+
+const container = document.getElementById("product-grid")
+
+data.forEach(product => {
+
+container.innerHTML += `
+<div>
+<img src="${product.image_url}" width="200">
+<h3>${product.name}</h3>
+<p>${product.price}</p>
+<a href="${product.affiliate_link}" target="_blank">Buy</a>
+</div>
+`
+
+})
+
+}
+
+loadProducts()
+
 </script>
 
